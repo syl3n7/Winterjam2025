1e16a13561064157fbadf66ce1c643dc4bb1e626
syl3n7 <3891084+syl3n7@users.noreply.github.com>
2025-02-15 23:21:02 +0000

    Refactor HealthUI to use Image array for heart display; remove unused heart setup logic

--- Assets/Scripts/HealthUI.cs
+++ Assets/Scripts/HealthUI.cs
@@ -5,14 +5,10 @@ using System.Collections.Generic;
 public class HealthUI : MonoBehaviour
 {
     [Header("Heart Settings")]
-    [SerializeField] private GameObject heartPrefab;
+    [SerializeField] private Image[] heartImages = new Image[3];
     [SerializeField] private Sprite fullHeartSprite;
     [SerializeField] private Sprite emptyHeartSprite;
-    [SerializeField] private Vector2 heartSize = new Vector2(50f, 50f);
-    [SerializeField] private float spacing = 10f;
-    [SerializeField] private Image[] heartImages = new Image[3];
 
-    private List<Image> hearts = new List<Image>();
     private RectTransform rectTransform;
 
     private void Awake()
@@ -23,12 +19,6 @@ public class HealthUI : MonoBehaviour
 
     private void Start()
     {
-        PlayerController player = FindObjectOfType<PlayerController>();
-        if (player != null)
-        {
-            SetupHearts(player.MaxHealth);
-        }
-
         // Make sure all hearts are full at start
         UpdateHearts(3);
     }
@@ -42,43 +32,8 @@ public class HealthUI : MonoBehaviour
         rectTransform.anchoredPosition = new Vector2(-20f, -20f); // 20 pixels from top-right
     }
 
-    private void SetupHearts(int maxHealth)
-    {
-        // Clear any existing hearts
-        foreach (Transform child in transform)
-        {
-            Destroy(child.gameObject);
-        }
-        hearts.Clear();
-
-        // Create hearts from right to left
-        for (int i = 0; i < maxHealth; i++)
-        {
-            GameObject heartObj = Instantiate(heartPrefab, transform);
-            RectTransform heartTransform = heartObj.GetComponent<RectTransform>();
-            
-            // Position from right to left
-            float xPos = -(heartSize.x + spacing) * i;
-            heartTransform.anchoredPosition = new Vector2(xPos, 0);
-            
-            Image heartImage = heartObj.GetComponent<Image>();
-            heartImage.sprite = fullHeartSprite;
-            heartImage.rectTransform.sizeDelta = heartSize;
-            hearts.Add(heartImage);
-        }
-
-        // Set container size
-        float totalWidth = (heartSize.x + spacing) * maxHealth - spacing;
-        rectTransform.sizeDelta = new Vector2(totalWidth, heartSize.y);
-    }
-
     public void UpdateHearts(int currentHealth)
     {
-        for (int i = 0; i < hearts.Count; i++)
-        {
-            hearts[i].sprite = i < currentHealth ? fullHeartSprite : emptyHeartSprite;
-        }
-
         for (int i = 0; i < heartImages.Length; i++)
         {
             heartImages[i].sprite = i < currentHealth ? fullHeartSprite : emptyHeartSprite;
